734e8fec1b0b15e9d3781f9e7fec33821c24160b
kayky8899 <kayky2108@gmail.com>
2026-06-04 00:02:41 -0300

    nova aula

---
+++ Exercicios/ex003/ex001.js
@@ -0,0 +1,6 @@
+var vel = 61
+console.log(`A valocidade do seu carro é ${vel}km/h`)
+    if ( vel > 60) {
+        console.log('voce ultrapassou a velocidade. MULTADO!')
+    }
+console.log('dirija sempre usando cinto de segurança')

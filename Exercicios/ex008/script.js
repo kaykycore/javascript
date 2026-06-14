677059cbd6cc225ba2eea7d3335732fbfa83a6c6
kayky8899 <kayky2108@gmail.com>
2026-06-13 22:14:53 -0300

    Vamos contar

--- Exercicios/ex008/script.js
+++ Exercicios/ex008/script.js
@@ -1,49 +1,19 @@
-function verificar() {
-    var data = new Date()
-    var ano = data.getFullYear()
-    var fano = document.getElementById('txtano')
-    var res = document.querySelector('div#res')
-    if (fano.value.length == 0 || Number(fano.value) > ano) {
-        window.alert('[ERRO] verifique os dados e tente novamente!')
-    } else {
-        var fsex = document.getElementsByName('radsex')
-        var idade = ano - Number(fano.value)
-        var gênero = ''
-        var img = document.createElement('img')
-        img.setAttribute('id', 'foto')
-        if (fsex[0].checked) {
-            gênero = 'Homem'
-            if (idade >=0 && idade < 10) {
-                //criança
-                img.setAttribute('src', 'imagens/bebe.jpg')
-            } else if (idade <21) {
-                //jovem
-                img.setAttribute('src', 'imagens/jovem.jpg')
-            } else if (idade < 50) {
-                //adulto
-                img.setAttribute('src', 'imagens/adulto.jpg')
-            } else {
-                //idoso
-                img.setAttribute('src', 'imagens/idoso.jpg')
+function contar() {
+    let ini = document.getElementById('txti')
+    let fim = document.getElementById('txtf')
+    let passo = document.getElementById('txtp')
+    let res = document.getElementById('res')
+
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        } else {
+            res.innerHTML = 'contando: '
+            let i = Number(ini.value)
+            let f = Number(fim.value)
+            let p = Number(passo.value)
+
+            for(let c = i; c <= f; c += p) {
+                res.innerHTML += `${c} \u{1F449}`
             }
-        } else if (fsex[1].checked) {
-            gênero = 'mulher'
-            if (idade >=0 && idade < 10) {
-                //criança
-                img.setAttribute('src', 'imagens/bebe.jpg')
-            } else if (idade <21) {
-                //jovem
-                img.setAttribute('src', 'imagens/jovem.jpg')
-            } else if (idade < 50) {
-                //adulto
-                img.setAttribute('src', 'imagens/adulto.jpg')
-            } else {
-                //idoso
-                img.setAttribute('src', 'imagens/idoso.jpg')
-            }
-        }
-        res.style.textAlign = 'center'
-        res.innerHTML = `Detectamos ${gênero} com ${idade} anos.`
-        res.appendChild(img)
+            res.innerHTML += `\u{1F3C1}`
+         }
     }
-}
